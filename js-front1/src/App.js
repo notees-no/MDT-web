16812505df178d249dc5aa4bc7d6600857e9f396
notees-no <skrebloegor@gmail.com>
2024-11-24 23:23:20 +0300

    Bug fixes

--- js-front1/src/App.js
+++ js-front1/src/App.js
@@ -3,7 +3,7 @@ import Router from './Router';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { lightTheme, darkTheme } from './theme';
-import Navbar from './pages/components/Navbar/Navbar';
+import Navbar from './pages/Navbar/Navbar';
 
 function App() {
   const [authenticatedUser, setAuthenticatedUser] = React.useState(null);
